f509a147b5682b643073e9cee6d86dc8ee5194bb
kaushiksamanta20 <kaushiksamanta17@gmail.com>
2022-10-03 21:37:36 +0530

    adding balanced paranthesis in c++

---
+++ balanced_parantheses.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+    bool ans=true;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+        {
+            st.push(s[i]);
+        }
+
+        else if(s[i]==')')
+        {
+            if(!st.empty() && st.top()=='(')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans=false;
+                break;
+            }
+        }
+
+        else if(s[i]=='{')
+        {
+            if(!st.empty() && st.top()=='}')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans=false;
+                break;
+            }
+        }
+
+        else if(s[i]==']')
+        {
+            if(!st.empty() && st.top()=='[')
+            {
+                st.pop();
+            }
+            else
+            {
+                ans=false;
+                break;
+            }
+        }
+    }
+    if(!st.empty())
+    {
+        ans=false;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string s="]";
+
+    if(isValid(s))
+    {
+        cout<<"the string is valid "<<endl;
+    }
+    else
+    {
+        cout<<"the string is not valid "<<endl;
+    }
+}
